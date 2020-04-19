ab9ba7084f0bd95d29f11360d7b85851abe1da3e
nlee192 <natashavlee@gmail.com>
2020-04-19 08:10:06 -0500

    Added some functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Natasha");
 
-        System.out.println("Hello, world!");
+        double len = 5;
+        System.out.println("Ploschad kvadrata so storonoy " + len + " = " + area(len));
 
-        System.out.println(2 + 2);
+        double a = 3;
+        double b = 4;
+        System.out.println("Ploschad pryamougolnika so storonami " + a + " i " + b + " = " + area(a,b));
+
+
+    }
+    public static void hello (String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area (double l) {
+        return l * l;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 
 }
