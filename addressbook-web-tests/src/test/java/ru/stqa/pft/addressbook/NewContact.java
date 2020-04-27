99ff59ab282ccf6aa986a0a1d6af6a264787adca
nlee192 <natashavlee@gmail.com>
2020-04-27 07:08:40 -0500

    NewContact: extracted methods and parameters, created new class NewContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContact.java
@@ -13,37 +13,60 @@ public class NewContact {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php#");
+    login("admin", "secret");
+  }
 
-    GroupCreationTests gct = new GroupCreationTests();
-    gct.login("admin", "secret");
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testNewContact() throws Exception {
-    
-    wd.findElement(By.linkText("add new")).click();
+
+    initNewContactCreation();
+    fillContactForm(new NewContactData("Natasha", "Lee", "(123)1234567", "na@le.com", "123 Terra ln, Richmond, TX"));
+    submitNewContact();
+    returnHomePage();
+  }
+
+  private void returnHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitNewContact() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactForm(NewContactData newContactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Natasha");
+    wd.findElement(By.name("firstname")).sendKeys(newContactData.getFirstname());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Lee");
+    wd.findElement(By.name("lastname")).sendKeys(newContactData.getLastname());
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("(123)1234567");
+    wd.findElement(By.name("home")).sendKeys(newContactData.getPhonenumber());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("na@le.com");
+    wd.findElement(By.name("email")).sendKeys(newContactData.getEmail());
     wd.findElement(By.name("address2")).click();
     wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys("123 Terra ln, Richmond, TX");
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    wd.findElement(By.linkText("home page")).click();
-    wd.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.name("address2")).sendKeys(newContactData.getAddress());
+  }
+
+  private void initNewContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
+    wd.findElement(By.linkText("Logout")).click();
     wd.quit();
   }
 
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class NewContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String phonenumber;
+  private final String email;
+  private final String address;
+
+  public NewContactData(String firstname, String lastname, String phonenumber, String email, String address) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.phonenumber = phonenumber;
+    this.email = email;
+    this.address = address;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getPhonenumber() {
+    return phonenumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+}
