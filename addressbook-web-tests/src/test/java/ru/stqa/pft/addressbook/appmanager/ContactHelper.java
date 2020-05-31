deda67b98068f51a1068af5f10fc3d1f8a1a96c8
nlee192 <natashavlee@gmail.com>
2020-05-31 11:56:51 -0500

    ContactEmailTests and ContactAddressTests created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -71,9 +71,14 @@ public class ContactHelper extends HelperBase {
     String home = wd.findElement(By.name("home")).getAttribute("value");
     String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
     String work = wd.findElement(By.name("work")).getAttribute("value");
+    String address = wd.findElement(By.name("address")).getAttribute("value");
+    String email1 = wd.findElement(By.name("email")).getAttribute("value");
+    String email2 = wd.findElement(By.name("email2")).getAttribute("value");
+    String email3 = wd.findElement(By.name("email3")).getAttribute("value");
     wd.navigate().back();
     return new ContactData().withId(id).withFirstname(firstname).withLastname(lastname)
-            .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
+            .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work).withAddress(address)
+            .withEmail1(email1).withEmail2(email2).withEmail3(email3);
   }
 
   public void updateContact() {
@@ -153,14 +158,14 @@ public class ContactHelper extends HelperBase {
       int id = Integer.parseInt(row.findElement(By.tagName("input")).getAttribute("value"));
       String lastname = cells.get(1).getText();
       String firstname = cells.get(2).getText();
-//      String address = cells.get(3).getText();
-//      String email = cells.get(4).getText();
+      String address = cells.get(3).getText();
+      String allEmails = cells.get(4).getText();
 //      String[] phones  = cells.get(5).getText().split("\n");
       String allPhones = cells.get(5).getText();
 
 
       contactsCache.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname)
-      .withAllPhones(allPhones));
+      .withAllPhones(allPhones).withAddress(address).withAllEmails(allEmails));
     }
     return contactsCache;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -11,63 +11,40 @@ public class ContactData {
   private String mobilephone;
   private String workphone;
   private String allPhones;
-  private String email;
   private String address;
+  private String email1;
+  private String email2;
+  private String email3;
+  private String allEmails;
   private String group;
 
   public int getId() {
     return id;
   }
-
-  public String getFirstname() {
-    return firstname;
-  }
-
-  public String getLastname() {
-    return lastname;
-  }
-
-  public String getHomephone() {
-    return homephone;
-  }
-
-  public String getMobilephone() {
-    return mobilephone;
-  }
-
-  public String getWorkPhone() {
-    return workphone;
-  }
-
-    public String getAllPhones() {
-    return allPhones;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public String getGroup() {
-    return group;
-  }
-
   public ContactData withId(int id) {
     this.id = id;
     return this;
   }
 
+  public String getFirstname() {
+    return firstname;
+  }
   public ContactData withFirstname(String firstname) {
     this.firstname = firstname;
     return this;
   }
 
+  public String getLastname() {
+    return lastname;
+  }
   public ContactData withLastname(String lastname) {
     this.lastname = lastname;
     return this;
+  }
+
+
+  public String getHomephone() {
+    return homephone;
   }
 
   public ContactData withHomePhone(String homephone) {
@@ -75,9 +52,17 @@ public class ContactData {
     return this;
   }
 
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
   public ContactData withMobilePhone(String mobilephone) {
     this.mobilephone = mobilephone;
     return this;
+  }
+
+  public String getWorkPhone() {
+    return workphone;
   }
 
   public ContactData withWorkPhone(String workphone) {
@@ -85,14 +70,52 @@ public class ContactData {
     return this;
   }
 
+  public String getAllPhones() {
+    return allPhones;
+  }
   public ContactData withAllPhones(String allPhones) {
     this.allPhones = allPhones;
     return this;
   }
 
-  public ContactData withEmail(String email) {
-    this.email = email;
+  public String getEmail1() {
+    return email1;
+  }
+
+  public ContactData withEmail1(String email1) {
+    this.email1 = email1;
     return this;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public ContactData withEmail2(String email2) {
+    this.email2 = email2;
+    return this;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+
+  public String getAllEmails() {
+    return allEmails;
+  }
+
+  public ContactData withAllEmails(String allEmails) {
+    this.allEmails = allEmails;
+    return this;
+  }
+
+  public ContactData withEmail3(String email3) {
+    this.email3 = email3;
+    return this;
+  }
+
+  public String getAddress() {
+        return address;
   }
 
   public ContactData withAddress(String address) {
@@ -100,6 +123,9 @@ public class ContactData {
     return this;
   }
 
+  public String getGroup() {
+    return group;
+  }
   public ContactData withGroup(String group) {
     this.group = group;
     return this;
@@ -111,6 +137,15 @@ public class ContactData {
             "id=" + id +
             ", firstname='" + firstname + '\'' +
             ", lastname='" + lastname + '\'' +
+            ", homephone='" + homephone + '\'' +
+            ", mobilephone='" + mobilephone + '\'' +
+            ", workphone='" + workphone + '\'' +
+            ", allPhones='" + allPhones + '\'' +
+            ", address='" + address + '\'' +
+            ", email1='" + email1 + '\'' +
+            ", email2='" + email2 + '\'' +
+            ", email3='" + email3 + '\'' +
+            ", group='" + group + '\'' +
             '}';
   }
 
