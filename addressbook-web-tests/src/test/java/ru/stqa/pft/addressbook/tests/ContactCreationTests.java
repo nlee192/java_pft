5254ff68fca978daa1e78b6282a7439a8d3ce78c
nlee192 <natashavlee@gmail.com>
2020-06-13 11:48:56 -0500

    ContactCreation: reading Data from xml file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,15 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,20 +20,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
+  //READING XML
   public Iterator<Object[]> validContacts() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line !=null) {
-      String[] split = line.split(";");
-      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1])});
+      xml += line;
       line = reader.readLine();
     }
-//    list.add(new Object[]{new ContactData().withFirstname("firstName1").withLastname("lastName1")});
-//    list.add(new Object[]{new ContactData().withFirstname("firstName2").withLastname("lastName2")});
-//    list.add(new Object[]{new ContactData().withFirstname("firstName3").withLastname("lastName3")});
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class); //reads annotations in GroupData class
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
   }
+
+//  //  READING CSV:
+//  public Iterator<Object[]> validContacts() throws IOException {
+//    List<Object[]> list = new ArrayList<Object[]>();
+//    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+//    String line = reader.readLine();
+//    while (line !=null) {
+//      String[] split = line.split(";");
+//      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1])});
+//      line = reader.readLine();
+//    }
+////    MANUAL DATA CREATION
+////    list.add(new Object[]{new ContactData().withFirstname("firstName1").withLastname("lastName1")});
+////    list.add(new Object[]{new ContactData().withFirstname("firstName2").withLastname("lastName2")});
+////    list.add(new Object[]{new ContactData().withFirstname("firstName3").withLastname("lastName3")});
+//    return list.iterator();
+//  }
 
   @Test(dataProvider = "validContacts")
   public void testNewContact(ContactData contact) throws Exception {
