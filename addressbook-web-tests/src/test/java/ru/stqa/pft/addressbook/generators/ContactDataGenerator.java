ea05c4fced483826a2ae1bb56138956f9b2a0211
nlee192 <natashavlee@gmail.com>
2020-06-13 11:43:51 -0500

    Added XStream to ContactDataGenerator. Creating data in xml file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -19,6 +20,9 @@ public class ContactDataGenerator {
 
   @Parameter(names = "-f", description = "Target file")
   public String file;
+
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
 
 
   public static void main(String[] args) throws IOException {
@@ -39,10 +43,26 @@ public class ContactDataGenerator {
 
   private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
-    save(contacts, new File (file));
+    if (format.equals("csv")) {
+      saveAsCsv(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognized format" + format);
+    }
   }
 
-  private void save(List<ContactData> contacts, File file) throws IOException {
+  private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class); //reads annotations in GroupData class
+//    xstream.alias("group", GroupData.class); works without annotations in GroupData class
+    String xml = xstream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact: contacts) {
       writer.write(String.format("%s;%s\n", contact.getFirstname(),contact.getLastname()));
